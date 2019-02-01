803553b0d4ed655142ae1dac12c24147d64fe005
hp <namstudionsk@gmail.com>
2019-02-02 01:24:03 +0700

    Add Comment Entity

---
+++ src/main/java/com/axmor/model/Comment.java
@@ -0,0 +1,60 @@
+package com.axmor.model;
+
+public class Comment {
+    private int id;
+    private int userId;
+    private int issueId;
+    private int statusId;
+    private String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(int issueId) {
+        this.issueId = issueId;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", issueId=" + issueId +
+                ", statusId=" + statusId +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
