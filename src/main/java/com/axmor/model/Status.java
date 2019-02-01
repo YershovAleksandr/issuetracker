17d05ac815edff61465311e6c8991c974c86ae14
hp <namstudionsk@gmail.com>
2019-02-02 01:24:29 +0700

    Add Status Entity

---
+++ src/main/java/com/axmor/model/Status.java
@@ -0,0 +1,11 @@
+package com.axmor.model;
+
+public class Status {
+    private int id;
+    private String status;
+    //TODO fix
+    //Available statuses:
+    //Created
+    //Resolved
+    //Closed
+}
