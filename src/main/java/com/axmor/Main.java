f2685f8a3526df6742c8f9dc029abf637785395c
hp <namstudionsk@gmail.com>
2019-02-02 01:25:59 +0700

    Wip Controller

--- src/main/java/com/axmor/Main.java
+++ src/main/java/com/axmor/Main.java
@@ -5,14 +5,38 @@ import spark.Response;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.staticFiles;
 
 /**
  * Application entry point
  */
 public class Main {
     public static void main(String[] args) {
-        port(80);
-        get("/", (Request req, Response res) ->
-                "<html><body><h1>Hello, world!</h1></body></html>");
+
+        staticFiles.location("/web");
+        port(8080);
+        get("/", (Request req, Response res) -> index());
+        //get("/viewissues", (Request req, Response res) -> {res.redirect("/index.html");});
+        get("/create", (Request req, Response res) -> create());
+        get("/comment", (Request req, Response res) -> comment());
+    }
+
+    private static String index(){
+        return "<html>" +
+                "<body>" +
+                "<h1>View Issues!</h1>" +
+                "<a href=\"/create\">Create Issue</a>" +
+                "<br>" +
+                "<a href=\"/create\">Comment Issue</a>" +
+                "</body>" +
+                "</html>";
+    }
+
+    private static String create(){
+        return "<html><body><h1>Create Issue</h1></body></html>";
+    }
+
+    private static String comment(){
+        return "<html><body><h1>Comment Issue</h1></body></html>";
     }
 }
